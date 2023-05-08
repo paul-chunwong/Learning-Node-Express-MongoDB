b3d742054a677599735bda172b083893edfe8b26
paul-chunwong <99629734+paul-chunwong@users.noreply.github.com>
2023-05-07 22:32:26 -0700

    connected to MongoDB

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
-// node server.js
-// npm run serve
+// $ node server.js
+// $ npm run serve
+// $ npm run dev
 
 
 
@@ -7,15 +8,28 @@
 
 const express = require('express')
 const app = express()
+const mongoose = require('mongoose');
 
-app.listen(3000, () => {
-    console.log("Node API App is running on port 3000")
-})
+
+// MongoDB database connection + node.js starts at port 3000
+mongoose
+.connect('mongodb+srv://admin:0306@cluster0.6bxg1zu.mongodb.net/?retryWrites=true&w=majority')
+.then(() => {
+    console.log('connected to MongoDB')
+    app.listen(3000, () => {
+        console.log("Node API App is running on port 3000")
+    })
+}).catch((error) => {
+    console.log(error)
+});
+
 
 // routes
 app.get('/', (req, res) => {
     res.send("Hello NODE API")
 })
 
-
+app.get('/blog', (req, res) => {
+    res.send("Hello BLOG")
+})
 
